5fe5a21044501061864dcaace9bea9dc96e65a38
uzzybotak <6913171+ahmadoji01@users.noreply.github.com>
2024-01-11 22:59:49 +0700

    ux(pos): Add Number Counter

--- modules/orders/application/list/order-item.list.tsx
+++ modules/orders/application/list/order-item.list.tsx
@@ -68,7 +68,21 @@ const OrderItemList = ({ handleDeleteModal }:OrderItemListProps) => {
             </div>
 
             <div className="hidden items-center justify-center sm:flex p-2.5 xl:p-5">
-              <p className="text-center text-black dark:text-white">{item.quantity}</p>
+              <div className="custom-number-input h-10">
+                <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
+                  <button className=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
+                    <span className="m-auto text-2xl font-thin">−</span>
+                  </button>
+                  <input 
+                    defaultValue={item.quantity}
+                    type="number" 
+                    className="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700 outline-none" 
+                    name="custom-input-number" />
+                  <button data-action="increment" className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
+                    <span className="m-auto text-2xl font-thin">+</span>
+                  </button>
+                </div>
+              </div>
             </div>
 
             <div className="hidden items-center justify-center sm:flex p-2.5 xl:p-5">
