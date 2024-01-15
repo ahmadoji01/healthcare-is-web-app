da8a7598eeb2067fa626492188ef8e3181cb1c87
uzzybotak <6913171+ahmadoji01@users.noreply.github.com>
2024-01-15 12:48:28 +0700

    build(pos): Add Tax Rate Config

---
+++ modules/payment-methods/application/cash.tsx
@@ -0,0 +1,14 @@
+
+interface CashProps {
+    total: number,
+    confirmPayment: () => void,
+}
+
+const CashPayment = ({ total, confirmPayment }: CashProps) => {
+    
+    return (
+        <></>
+    );
+}
+
+export default CashPayment;
