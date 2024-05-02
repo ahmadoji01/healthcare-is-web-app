d993b28a3876f88619542b9e732dd41d056588d7
uzzybotak <6913171+ahmadoji01@users.noreply.github.com>
2024-05-02 12:48:36 +0700

    fix(dashboard): Add Refresh Token Wait to Load on App Layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const {loading} = useUserContext();
   const [theme, setTheme] = useState(createTheme({ palette: { mode: "light" } }));
+  const {loading} = useUserContext();
 
   useEffect(() => {
     const onStorageChange = () => {
@@ -32,29 +32,31 @@ export default function RootLayout({
     <html lang="en">
       <body suppressHydrationWarning={true}>
         <div className="dark:bg-boxdark-2 dark:text-bodydark">
-          {loading ? (
-            <Loader />
-          ) : (
-            <div className="flex h-screen overflow-hidden">
-              <Sidebar
-                sidebarOpen={sidebarOpen}
-                setSidebarOpen={setSidebarOpen}
-              />
-              <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-                <Header
+          <div className="flex h-screen overflow-hidden">
+            {loading ? (
+              <Loader />
+            ) : (
+              <>
+                <Sidebar
                   sidebarOpen={sidebarOpen}
                   setSidebarOpen={setSidebarOpen}
                 />
-                <ThemeProvider theme={theme} >
-                  <main>
-                    <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-                      {children}
-                    </div>
-                  </main>
-                </ThemeProvider>
-              </div>
-            </div>
-          )}
+                <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                  <Header
+                    sidebarOpen={sidebarOpen}
+                    setSidebarOpen={setSidebarOpen}
+                  />
+                  <ThemeProvider theme={theme} >
+                    <main>
+                      <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                        {children}
+                      </div>
+                    </main>
+                  </ThemeProvider>
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </body>
     </html>
