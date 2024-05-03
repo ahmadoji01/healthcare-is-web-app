55c5a7318bd2b3dc520f01c4c17e87050b9a3001
uzzybotak <6913171+ahmadoji01@users.noreply.github.com>
2024-05-03 22:10:36 +0700

    feature(dashboard): Create Medicine Form

--- modules/medicines/domain/medicines.actions.tsx
+++ modules/medicines/domain/medicines.actions.tsx
@@ -1,5 +1,18 @@
 import { LIMIT_PER_PAGE } from "@/constants/request";
 import { directusClient } from "@/utils/request-handler"
-import { readItems, withToken } from "@directus/sdk";
+import { createItem, readItems, withToken } from "@directus/sdk";
+import { MedicineNoID } from "./medicine";
 
 export const getAllMedicines = (token:string, page:number) => directusClient.request( withToken(token, readItems('medicines', { fields: ['*.*'], limit: LIMIT_PER_PAGE, page })) );
+export const medicineExistChecker = (token:string, name = "") => 
+	directusClient.request( 
+		withToken(token, readItems('medicines', { 
+			filter: {
+				name: { _eq: name }
+			} 
+		})) 
+	)
+
+export const createAMedicine = (token:string, medicine:MedicineNoID) => 
+    directusClient.request( withToken(token, createItem('medicines', medicine)) )
+    
