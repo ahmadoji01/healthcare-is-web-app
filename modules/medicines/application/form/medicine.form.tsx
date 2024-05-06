a8968bda5e205750410ebf5316f6d7ffbfc85c2c
uzzybotak <6913171+ahmadoji01@users.noreply.github.com>
2024-05-06 10:42:50 +0700

    ux(dashboard): Add Medicine Form

---
+++ modules/medicines/application/form/medicine.form.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import SubmitButton from '@/components/Dashboard/Submit';
+import { Medicine } from '../../domain/medicine';
+import { useState } from 'react';
+
+interface MedicineFormProps {
+    initMedicine: Medicine,
+    handleSubmit: (medicine:Medicine) => void,
+}
+
+const MedicineForm = ({ initMedicine, handleSubmit }:MedicineFormProps) => {
+    const [medicine, setMedicine] = useState(initMedicine);
+
+    return (
+        <>
+            <div className="grid gap-9">
+                <form onSubmit={e => { e.preventDefault(); handleSubmit(medicine) } }>
+                    <div className="flex flex-col gap-9">
+                        <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+                            <div className="flex flex-col gap-5.5 p-6.5">
+                                <div>
+                                    <label className="mb-3 block text-black dark:text-white">
+                                        Name
+                                    </label>
+                                    <input
+                                        type="text" 
+                                        defaultValue={medicine.name}
+                                        required
+                                        onChange={ e => setMedicine({ ...medicine, name: e.target.value })}
+                                        placeholder="Input Your Full Name"
+                                        className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                                        />
+                                </div>
+                                <div>
+                                    <label className="mb-3 block text-black dark:text-white">
+                                        Stock
+                                    </label>
+                                    <div className="relative">
+                                    <input
+                                        type="number"
+                                        defaultValue={medicine.stock}
+                                        required
+                                        onChange={ e => setMedicine({ ...medicine, stock: parseInt(e.target.value) })}
+                                        placeholder="Input the Stock"
+                                        className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                                        />
+                                    </div>
+                                </div>
+                                <div>
+                                    <label className="mb-3 block text-black dark:text-white">
+                                        Price
+                                    </label>
+                                    <div className="relative">
+                                    <input
+                                        type="number"
+                                        defaultValue={medicine.price}
+                                        required
+                                        onChange={ e => setMedicine({ ...medicine, price: parseInt(e.target.value) })}
+                                        placeholder="Input the Price"
+                                        className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <SubmitButton />
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default MedicineForm;
