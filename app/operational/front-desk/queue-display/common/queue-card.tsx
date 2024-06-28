12c3e6f453725bfb20535af2ad503fadfcb9aece
uzzybotak <6913171+ahmadoji01@users.noreply.github.com>
2024-06-28 20:09:06 +0700

    ux: Change Queue Card Size

--- app/operational/front-desk/queue-display/common/queue-card.tsx
+++ app/operational/front-desk/queue-display/common/queue-card.tsx
@@ -6,8 +6,8 @@ const QueueCard = ({ number=0, name="" }) => {
     const {t} = useTranslation();
 
     return (
-        <div className="w-1/3 p-6 flex-col">
-            <Card sx={{ minWidth: 275 }}>
+        <div className="w-1/6 p-2 flex-col">
+            <Card sx={{ minWidth: 100, minHeight: 190 }}>
                 <CardContent>
                     <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                         {t('queue_number')}
@@ -15,7 +15,7 @@ const QueueCard = ({ number=0, name="" }) => {
                     <Typography variant="h2" fontWeight="700" component="div" textAlign="center">
                         {number}
                     </Typography>
-                    <Typography variant="h5" fontWeight="400" textAlign="center">
+                    <Typography variant="body2" fontWeight="400" textAlign="center">
                         {name}
                     </Typography>
                 </CardContent>
